17d0aea67f737acb32b6268b4c21160e78434129
Breno Tostes <bttostes@dcc.ufrj.br>
2024-09-14 21:06:01 -0300

    FIX Added check beore fetch instance

--- ORLibrary_SPC/fetch_instance.py
+++ ORLibrary_SPC/fetch_instance.py
@@ -8,7 +8,17 @@ def get_data(instance_url: str):
 
     # Extrai o nome da instancia do URL
     instance_name = instance_url.split('/')[-1]
-    
+
+    try:
+        file_path = f'Instancias/{instance_name}'
+
+        with open(file_path, 'r') as file:
+            print(f'{instance_name} already found at: {file_path}')
+            return
+    except Exception as e:
+        print(f"Error: {e}")
+        print(f'Could not find {instance_name}, fetching from source...')
+
     # Envia uma requisição HTTP GET para pegar os dados 
     response = requests.get(instance_url)
     
@@ -24,7 +34,7 @@ def get_data(instance_url: str):
         print(f"Failed to fetch data from {instance_url}. Status code: {response.status_code}")
 
 if __name__ == "__main__":
-    instance_name = input("Unit test for fetching instance data. Input the instance code:")
+    instance_name = 'scp41' # input("Unit test for fetching instance data. Input the instance code:")
     
-    url = f"http://people.brunel.ac.uk/~mastjjb/jeb/orlib/files/scp{instance_name}.txt"
+    url = f"http://people.brunel.ac.uk/~mastjjb/jeb/orlib/files/{instance_name}.txt"
     get_data(instance_url=url)
